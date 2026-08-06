4d5d497185c5496dd32e5209d3f9d14b71e8f285
Shivnil90 <shivnil90@gmail.com>
2026-08-06 18:47:47 +1000

    added new folders

---
+++ Lab Exercise 4/exercise4.js
@@ -0,0 +1,12 @@
+function greet(name) {
+2
+console.log("Hello " + name);
+3
+}
+4
+ 
+5
+// Call the function
+6 
+greet("SHIVNIL");
+
---
+++ Lab Exercise 5/exercise5.js
@@ -0,0 +1,11 @@
+let fruits = ["Apple", "Banana", "Orange", "Mango", "Grapes"];
+
+console.log(fruits[0]); // Apple
+console.log(fruits[4]); // Grapes
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+let numbers = [10, 20, 30, 40, 50];
+
